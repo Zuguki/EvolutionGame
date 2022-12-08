29375563e93a7dc5b3771548393ca2f16decad76
Zuguki <guskov.zahar20@gmail.com>
2022-12-08 10:21:33 +0500

    Settings controller was added

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,11 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SceneLoader : MonoBehaviour
 {
-    [SerializeField]
-    private  GameObject weatherSettingsObject;
+    [SerializeField] private GameObject weatherSettingsObject;
 
     public void ChangeWeatherSettingsActiveStatus() =>
         weatherSettingsObject.SetActive(!weatherSettingsObject.activeSelf);
---
+++ Assets/Scripts/SettingsController.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsController : MonoBehaviour
+{
+    private TMP_InputField _temperatureValue;
+    private Slider _temperatureSlider;
+    private TMP_InputField _pressureValue;
+    private Slider _pressureSlider;
+    private TMP_InputField _radiationValue;
+    private Slider _radiationSlider;
+    private TMP_InputField _humidityValue;
+    private Slider _humiditySlider;
+    private TMP_InputField _windSpeedValue;
+    private Slider _windSpeedSlider;
+    private TMP_InputField _preciptiationValue;
+    private Slider _preciptioationSlider;
+
+    private void Awake()
+    {
+        _temperatureValue = transform.GetChild(1).GetChild(2).GetComponent<TMP_InputField>();
+        _temperatureSlider = transform.GetChild(1).GetChild(1).GetComponent<Slider>();
+        _pressureValue = transform.GetChild(2).GetChild(2).GetComponent<TMP_InputField>();
+        _pressureSlider = transform.GetChild(2).GetChild(1).GetComponent<Slider>();
+        _radiationValue = transform.GetChild(3).GetChild(2).GetComponent<TMP_InputField>();
+        _radiationSlider = transform.GetChild(3).GetChild(1).GetComponent<Slider>();
+        _humidityValue = transform.GetChild(4).GetChild(2).GetComponent<TMP_InputField>();
+        _humiditySlider = transform.GetChild(4).GetChild(1).GetComponent<Slider>();
+        _windSpeedValue = transform.GetChild(5).GetChild(2).GetComponent<TMP_InputField>();
+        _windSpeedSlider = transform.GetChild(5).GetChild(1).GetComponent<Slider>();
+        _preciptiationValue = transform.GetChild(6).GetChild(2).GetComponent<TMP_InputField>();
+        _preciptioationSlider = transform.GetChild(6).GetChild(1).GetComponent<Slider>();
+    }
+
+    public void Start()
+    {
+        _temperatureValue.onValueChanged.AddListener((value) => _temperatureSlider.value = ParseFloat(value));
+        _temperatureSlider.onValueChanged.AddListener((value) =>
+            _temperatureValue.text = value.ToString(CultureInfo.InvariantCulture));
+        _pressureValue.onValueChanged.AddListener((value) => _pressureSlider.value = ParseFloat(value));
+        _pressureSlider.onValueChanged.AddListener((value) =>
+            _pressureValue.text = value.ToString(CultureInfo.InvariantCulture));
+        _radiationValue.onValueChanged.AddListener((value) => _radiationSlider.value = ParseFloat(value));
+        _radiationSlider.onValueChanged.AddListener((value) =>
+            _radiationValue.text = value.ToString(CultureInfo.InvariantCulture));
+        _humidityValue.onValueChanged.AddListener((value) => _humiditySlider.value = ParseFloat(value));
+        _humiditySlider.onValueChanged.AddListener((value) =>
+            _humidityValue.text = value.ToString(CultureInfo.InvariantCulture));
+        _windSpeedValue.onValueChanged.AddListener((value) => _windSpeedSlider.value = ParseFloat(value));
+        _windSpeedSlider.onValueChanged.AddListener((value) =>
+            _windSpeedValue.text = value.ToString(CultureInfo.InvariantCulture));
+        _preciptiationValue.onValueChanged.AddListener((value) => _preciptioationSlider.value = ParseFloat(value));
+        _preciptioationSlider.onValueChanged.AddListener((value) =>
+            _preciptiationValue.text = value.ToString(CultureInfo.InvariantCulture));
+    }
+
+    private float ParseFloat(string value)
+    {
+        var isFloat = float.TryParse(value, out var result);
+        if (isFloat)
+            return result;
+        
+        _temperatureValue.text = _temperatureSlider.value.ToString(CultureInfo.InvariantCulture);
+        return _temperatureSlider.value;
+    }
+}
