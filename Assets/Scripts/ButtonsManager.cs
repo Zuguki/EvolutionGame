07682e10c9052cc2345465393f3124b4da90b0a4
Zuguki <guskov.zahar20@gmail.com>
2023-01-15 16:52:41 +0500

    EscMenu was added

--- Assets/Scripts/ButtonsManager.cs
+++ Assets/Scripts/ButtonsManager.cs
@@ -5,6 +5,7 @@ public class ButtonsManager : MonoBehaviour
     [SerializeField] private GameObject weatherSettingsObject;
     [SerializeField] private GameObject timeLineObject;
     [SerializeField] private GameObject populationMenu;
+    [SerializeField] private GameObject escMenu;
 
     public void ChangeWeatherSettingsActiveStatus() =>
         weatherSettingsObject.SetActive(!weatherSettingsObject.activeSelf);
@@ -18,7 +19,15 @@ public class ButtonsManager : MonoBehaviour
         else
             DrawPopulationMenu.DrawObjects();
         populationMenu.SetActive(!populationMenu.activeSelf);  
-    } 
+    }
+
+    public void ChangeEscMenuActiveStatus()
+    {
+        escMenu.SetActive(!escMenu.activeSelf);
+        PauseMenu.IsGamePaused = !PauseMenu.IsGamePaused;
+    }
+
+    public void CloseGame() => Application.Quit();
 
     public void StartIteration() => Program.Run();
 }
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+
+    public static bool IsGamePaused;
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        IsGamePaused = false;
+    }
+
+    private void Update()
+    {
+        if (!Input.GetKeyDown(KeyCode.Escape))
+            return;
+
+        if (IsGamePaused)
+            Resume();
+        else
+            Pause();
+    }
+
+    private void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        IsGamePaused = true;
+    }
+}
